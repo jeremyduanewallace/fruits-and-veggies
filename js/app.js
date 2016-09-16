c4efe03539cda94fefd12a83473871776017c75f
jeremyduanewallace <jeremyduanewallace@gmail.com>
2016-09-16 08:28:24 -0700

    app and controlller

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 /* setup your angular app here */
+var app = angular.module("myModule", []);
+
+// setup your controller here
+var myController = function($scope){
+  $scope.message = "Hello Angular";
+  
+}
+
+// register your controller with the module here
+app.controller("myController", myController);
 
 // debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
